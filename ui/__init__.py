aea8094ba788492c6ae43861f7e55192591096c9
Luigi Pirelli <luipir@gmail.com>
2015-04-20 11:20:34 +0200

    added

---
+++ ui/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ QTraffic
+                                 A QGIS plugin
+ Road contamination modelling for EMSURE Project
+                             -------------------
+        begin                : 2015-04-20
+        copyright            : (C) 2015 by Luigi Pirelli (for EMSURE project)º
+        email                : luipir@gmail.lcom
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
