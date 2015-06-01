481358ac035329f5881cd9dcf2fb81df1a116f2a
Luigi Pirelli <luipir@gmail.com>
2015-06-01 12:04:18 +0200

    added header and license

--- testdata/VehicleDistributionClasses/convertFleetDistrib.py
+++ testdata/VehicleDistributionClasses/convertFleetDistrib.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ QTraffic
+ A QGIS plugin Road contamination modelling for EMSURE Project
+                              -------------------
+        begin                : 2015-04-20
+        git sha              : $Format:%H$
+        copyright            : (C) 2015 by Luigi Pirelli (for EMSURE project)
+        email                : luipir@gmail.lcom
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
 import json
 import collections
 
