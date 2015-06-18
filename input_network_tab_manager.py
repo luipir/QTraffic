79136274417e30eb888283f2fc10018d202ef4ad
Luigi Pirelli <luipir@gmail.com>
2015-06-18 13:53:54 +0200

    merged tabs input network and vechick count

--- input_network_tab_manager.py
+++ input_network_tab_manager.py
@@ -140,7 +140,84 @@ class InputNetworkTabManager(QtCore.QObject):
         self.gui.roadType_CBox.currentIndexChanged.connect(self.saveTabOnProject)
         self.gui.roadLenght_CBox.currentIndexChanged.connect(self.saveTabOnProject)
         self.gui.roadGradient_CBox.currentIndexChanged.connect(self.saveTabOnProject)
+        
+        
+        #
+        #  set parameters for vechicle count/speed columns
+        #
+        # get parameters fron project
+        columnPassengerCars = self.project.value('VehicleCountSpeed/columnPassengerCars', '')
+        columnLightDutyVehicle = self.project.value('VehicleCountSpeed/columnLightDutyVehicle', '')
+        columnHeavyDutyVechicle = self.project.value('VehicleCountSpeed/columnHeavyDutyVechicle', '')
+        columnUrbanBuses = self.project.value('VehicleCountSpeed/columnUrbanBuses', '')
+        
+        columnMotorcycle = self.project.value('VehicleCountSpeed/columnMotorcycle', '')
+        columnCouch = self.project.value('VehicleCountSpeed/columnCouch', '')
+        columnAverageSpeed = self.project.value('VehicleCountSpeed/columnAverageSpeed', '')
+        
+        # now populare combo boxes with layer colums
+        fieldNames = sorted([field.name() for field in self.roadLayer.pendingFields().toList()])
+        unknownIndex = -1
+        
+        self.gui.passengerCarsCount_CBox.clear()
+        self.gui.lightDutyVehicleCount_CBox.clear()
+        self.gui.heavyDutyVehicleCount_CBox.clear()
+        self.gui.urbanBusesCount_CBox.clear()
+        self.gui.coachesCount_CBox.clear()
+        self.gui.motorcycleCount_CBox.clear()
+        self.gui.averageVehicleSpeed_Cbox.clear()
+        
+        self.gui.passengerCarsCount_CBox.addItems(fieldNames)
+        self.gui.lightDutyVehicleCount_CBox.addItems(fieldNames)
+        self.gui.heavyDutyVehicleCount_CBox.addItems(fieldNames)
+        self.gui.urbanBusesCount_CBox.addItems(fieldNames)
+        self.gui.coachesCount_CBox.addItems(fieldNames)
+        self.gui.motorcycleCount_CBox.addItems(fieldNames)
+        self.gui.averageVehicleSpeed_Cbox.addItems(fieldNames)
+        
+        # select the combobox item as in the project file... if not available then "Please select"
+        index = self.gui.passengerCarsCount_CBox.findText(columnPassengerCars)
+        self.gui.passengerCarsCount_CBox.setCurrentIndex( index if index >= 0 else unknownIndex )
+        
+        index = self.gui.lightDutyVehicleCount_CBox.findText(columnLightDutyVehicle)
+        self.gui.lightDutyVehicleCount_CBox.setCurrentIndex( index if index >= 0 else unknownIndex )
+        
+        index = self.gui.heavyDutyVehicleCount_CBox.findText(columnHeavyDutyVechicle)
+        self.gui.heavyDutyVehicleCount_CBox.setCurrentIndex( index if index >= 0 else unknownIndex )
+        
+        index = self.gui.urbanBusesCount_CBox.findText(columnUrbanBuses)
+        self.gui.urbanBusesCount_CBox.setCurrentIndex( index if index >= 0 else unknownIndex )
+        
+        index = self.gui.coachesCount_CBox.findText(columnCouch)
+        self.gui.coachesCount_CBox.setCurrentIndex( index if index >= 0 else unknownIndex )
+        
+        index = self.gui.motorcycleCount_CBox.findText(columnMotorcycle)
+        self.gui.motorcycleCount_CBox.setCurrentIndex( index if index >= 0 else unknownIndex )
+        
+        index = self.gui.averageVehicleSpeed_Cbox.findText(columnAverageSpeed)
+        self.gui.averageVehicleSpeed_Cbox.setCurrentIndex( index if index >= 0 else unknownIndex )
     
+        # add all modification events to notify project modification
+        try:
+            # to avoid add multiple listener, remove previous listener
+            self.gui.passengerCarsCount_CBox.currentIndexChanged.disconnect(self.saveTabOnProject)
+            self.gui.lightDutyVehicleCount_CBox.currentIndexChanged.disconnect(self.saveTabOnProject)
+            self.gui.heavyDutyVehicleCount_CBox.currentIndexChanged.disconnect(self.saveTabOnProject)
+            self.gui.urbanBusesCount_CBox.currentIndexChanged.disconnect(self.saveTabOnProject)
+            self.gui.coachesCount_CBox.currentIndexChanged.disconnect(self.saveTabOnProject)
+            self.gui.motorcycleCount_CBox.currentIndexChanged.disconnect(self.saveTabOnProject)
+            self.gui.averageVehicleSpeed_Cbox.currentIndexChanged.disconnect(self.saveTabOnProject)
+        except (Exception) as ex:
+            pass
+        
+        self.gui.passengerCarsCount_CBox.currentIndexChanged.connect(self.saveTabOnProject)
+        self.gui.lightDutyVehicleCount_CBox.currentIndexChanged.connect(self.saveTabOnProject)
+        self.gui.heavyDutyVehicleCount_CBox.currentIndexChanged.connect(self.saveTabOnProject)
+        self.gui.urbanBusesCount_CBox.currentIndexChanged.connect(self.saveTabOnProject)
+        self.gui.coachesCount_CBox.currentIndexChanged.connect(self.saveTabOnProject)
+        self.gui.motorcycleCount_CBox.currentIndexChanged.connect(self.saveTabOnProject)
+        self.gui.averageVehicleSpeed_Cbox.currentIndexChanged.connect(self.saveTabOnProject)
+        
     def saveTabOnProject(self):
         ''' Save tab configuration in the project basing on GUI values
         '''
@@ -156,6 +233,29 @@ class InputNetworkTabManager(QtCore.QObject):
         self.project.setValue('InputNetwork/columnRoadLenght', columnRoadLenght)
         self.project.setValue('InputNetwork/columnRoadSlope', columnRoadSlope)
         
+        #
+        #  save parameters for vechicle count/speed columns
+        #
+        # get values from the GUI
+        columnPassengerCars = self.gui.passengerCarsCount_CBox.currentText()
+        columnLightDutyVehicle = self.gui.lightDutyVehicleCount_CBox.currentText()
+        columnHeavyDutyVechicle = self.gui.heavyDutyVehicleCount_CBox.currentText()
+        columnUrbanBuses = self.gui.urbanBusesCount_CBox.currentText()
+        
+        columnCouch = self.gui.coachesCount_CBox.currentText()
+        columnMotorcycle = self.gui.motorcycleCount_CBox.currentText()
+        columnAverageSpeed = self.gui.averageVehicleSpeed_Cbox.currentText()
+        
+        # set conf parameters
+        self.project.setValue('VehicleCountSpeed/columnPassengerCars', columnPassengerCars)
+        self.project.setValue('VehicleCountSpeed/columnLightDutyVehicle', columnLightDutyVehicle)
+        self.project.setValue('VehicleCountSpeed/columnHeavyDutyVechicle', columnHeavyDutyVechicle)
+        self.project.setValue('VehicleCountSpeed/columnUrbanBuses', columnUrbanBuses)
+        self.project.setValue('VehicleCountSpeed/columnMotorcycle', columnMotorcycle)
+        self.project.setValue('VehicleCountSpeed/columnCouch', columnCouch)
+        self.project.setValue('VehicleCountSpeed/columnAverageSpeed', columnAverageSpeed)
+        
+        # notify project modification
         self.projectModified.emit()
         
     def getRoadLayer(self):
