b68a454c995b0b4070407494bc595f7e8bd25900
Luigi Pirelli <luipir@gmail.com>
2015-07-07 12:52:53 +0200

    minor gui reorg

--- ui/qtraffic_dialog_base_ui.py
+++ ui/qtraffic_dialog_base_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/qtraffic_dialog_base.ui'
 #
-# Created: Mon Jul  6 17:27:21 2015
+# Created: Tue Jul  7 12:50:12 2015
 #      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!
